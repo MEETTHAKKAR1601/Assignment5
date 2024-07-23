7b6e1a46285c4f0e8efe500573a5685b4a2beffe
neelrami13 <neelrami49@gmail.com>
2024-07-23 15:13:52 -0400

    Third Commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
-const collegeData = require('./collegeData.js');
+const collegeData = require('./collegeDATA.js');
 
 const app = express();
 const PORT = process.env.PORT || 8080;
